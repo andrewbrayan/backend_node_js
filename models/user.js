832189f60eafccac13c7ab6e597ed4ca9d27110f
Brayan Martinez <bamartinez@econtainers.co>
2022-08-17 21:53:26 -0500

    Se retorna usuario

--- models/user.js
+++ models/user.js
@@ -16,7 +16,6 @@ userSchema.methods.toJSON = function () {
   var userObject = this.toObject();
   delete userObject.password;
   delete userObject.email;
-  delete userObject.role;
   return userObject;
 };
 
